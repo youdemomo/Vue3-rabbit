44b390379d264f539e555101cf8c8eecaf731e96
Zhong Ting Qiang <2861315501@qq.com>
2025-02-28 16:06:15 +0800

    6. scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView />
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .test {
+    color: $priceColor;
+  }
+</style>
