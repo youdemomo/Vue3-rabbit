42373697819c35d6669b1e1efe847e357ffdd952
Zhong Ting Qiang <2861315501@qq.com>
2025-03-01 19:08:19 +0800

    22. 实现二级分类轮播图

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -3,6 +3,7 @@
     import { onMounted, ref } from 'vue';
     import { useRoute } from 'vue-router';
     import { getBannerAPI } from '@/apis/home'
+    import GoodsItem from '../Home/components/GoodsItem.vue';
 
     // 获取数据
     const categoryData = ref({})
@@ -49,6 +50,29 @@
                         <img :src="item.imgUrl" alt="接口大抵是寄了">
                     </el-carousel-item>
                 </el-carousel>
+            </div>
+
+            <!-- 分类商品列表 -->
+            <div class="sub-list">
+                <h3>全部分类</h3>
+                <ul>
+                    <li v-for="i in categoryData.children" :key="i.id">
+                        <RouterLink to="/">
+                            <img :src="i.picture" />
+                            <p>{{ i.name }}</p>
+                        </RouterLink>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- 二级商品列表 -->
+            <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+                <div class="head">
+                    <h3>- {{ item.name }}-</h3>
+                </div>
+                <div class="body">
+                    <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+                </div>
             </div>
 
         </div>
