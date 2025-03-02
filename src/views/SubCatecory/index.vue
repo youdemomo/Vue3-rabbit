4e24fe75c88eeba251bd4137f78648ea4d06f79c
Zhong Ting Qiang <2861315501@qq.com>
2025-03-02 16:32:37 +0800

    29. 完成二级分类列表的滚动加载

--- src/views/SubCatecory/index.vue
+++ src/views/SubCatecory/index.vue
@@ -4,7 +4,7 @@
     import { useRoute } from 'vue-router';
     import GoodsItem from '../Home/components/GoodsItem.vue';
 
-    // bro: 获取面包屑导航数据
+    // todo: 获取面包屑导航数据
     const route = useRoute()
     const categoryData = ref({})
     const getCategoryData = async () => {
@@ -16,7 +16,7 @@
 
     onMounted(() => getCategoryData())
 
-    // bro: 获取基础列表数据
+    // todo: 获取基础列表数据
     const goodsList = ref([])
     const reqData = ref({
         categoryId: route.params.id,
@@ -31,6 +31,31 @@
     }
 
     onMounted(() => getGoodList())
+
+    // todo: tab切换回调
+    const tabChange = () => {
+        // console.log('tab切换了', reqData.value);
+        // 重置页数
+        reqData.value.page = 1
+        getGoodList()
+    }
+
+    // todo: 滚动到底部时加载更多
+    const disable = ref(false)
+    const load = async () => {
+        console.log('加载更多');
+        // 获取下一页数据
+        reqData.value.page++
+        const res = await getSubCategoryAPI(reqData.value)
+
+        // 新老数据拼接
+        goodsList.value = [...goodsList.value, ...res.result.items]
+
+        // 加载完毕，停止监听
+        if (res.result.items.length === 0)
+            disable.value = true
+
+    }   
 </script>
 
 <template>
@@ -44,15 +69,21 @@
                 <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
+
+        <!-- 商品列表筛选栏 -->
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
-                <!-- 商品列表-->
+
+            <!-- 商品列表-->
+            <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disable">
                 <GoodsItem v-for="good in goodsList" :key="good.id" :good="good" />
+                <el-divider v-if="disable">
+                    翻到底了≧ ﹏ ≦
+                </el-divider>
             </div>
         </div>
     </div>
