4c1e057967e21a419f414566ec408b3091d4cfd0
Zhong Ting Qiang <2861315501@qq.com>
2025-03-02 19:42:01 +0800

    36. 实现滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-    import { ref } from 'vue'
+    import { ref, watch } from 'vue'
+    import { useMouseInElement } from '@vueuse/core'
 
     // 图片列表
     const imageList = [
@@ -15,19 +16,54 @@
     const activeIndex = ref(0)
     const enterHandler = i => {
         activeIndex.value = i
-        console.log(activeIndex.value);
+        // console.log(activeIndex.value);
     }
+
+    // todo: VueUse实现图片放大效果
+    const target = ref(null)
+    const { elementX, elementY, isOutside } = useMouseInElement(target)
+    const left = ref(0)
+    const top = ref(0)
+
+    // 监听鼠标坐标的变化
+    watch([elementX, elementY], () => {
+        // console.log('xy变化了');
+        // 横向
+        if (elementX.value > 100 && elementX.value < 300) {
+            left.value = elementX.value - 100
+        }
+        // 纵向
+        if (elementY.value > 100 && elementY.value < 300) {
+            top.value = elementY.value - 100
+        }
+
+        // 处理边界
+        if (elementX.value > 300) {
+            left.value = 200
+        }
+        if (elementX.value < 100) {
+            left.value = 0
+        }
+
+        if (elementY.value > 300) {
+            top.value = 200
+        }
+        if (elementY.value < 100) {
+            top.value = 0
+        }
+    })
 </script>
 
 
 <template>
+    {{ elementX }}, {{ elementY }}, {{ isOutside }}
     <div class="goods-image">
         <!-- 左侧大图-->
         <div class="middle" ref="target">
             <!-- 显示鼠标停留的小图 -->
             <img :src="imageList[activeIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+            <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
