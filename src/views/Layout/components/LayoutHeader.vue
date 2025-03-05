106ae8a7b7c1934c1c1bc1e61d88698345249c42
Zhong Ting Qiang <2861315501@qq.com>
2025-03-05 17:11:06 +0800

    52. 导航栏购物车按钮列表渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -2,6 +2,7 @@
     import { getCategoryAPI } from '@/apis/layout'
     import { ref } from 'vue'
     import { useCategoryStore } from '@/stores/category'
+    import HeaderCart from './HeaderCart.vue'
 
     const categoryStore = useCategoryStore()
 </script>
@@ -25,7 +26,7 @@
                 <input type="text" placeholder="搜一搜">
             </div>
             <!-- 头部购物车 -->
-
+            <HeaderCart />
         </div>
     </header>
 </template>
